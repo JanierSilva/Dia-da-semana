a6ea5afeda691defcc052e27232e1217c5c85e61
Janier Silva <janier.silva@grupoicts.com.br>
2022-09-14 16:40:38 -0400

    aprendendo sobre funções, métodos e parâmetros 2

--- src/Funcoes.java
+++ src/Funcoes.java
@@ -1,10 +1,18 @@
 public class Funcoes {
-    public static void main (String [] args){
-        String nomeOriginal = "Janier Silva";
-        saudacao(nomeOriginal);
-    }
-    public static void saudacao(String nomeParametro){
+    //public static void main (String [] args){
+        //String nomeOriginal = "Janier Silva";
+        //saudacao(nomeOriginal);
 
-        System.out.println("Hello, " + nomeParametro);
+    public static void main(String [] args){
+        int resultado = soma(2, 3);
+        System.out.println(resultado);
     }
-}
+    //public static void saudacao(String nomeParametro){
+
+        //System.out.println("Hello, " + nomeParametro);
+
+    public static int soma(int a, int b){
+        return a + b;
+    }
+    }
+
