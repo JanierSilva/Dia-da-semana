b743c7086f17209d3a4011c7ea15211849c47363
Janier Silva <janier.silva@grupoicts.com.br>
2022-09-15 10:47:22 -0400

    criação de uma tabuada utilizando o input para interação com o usuário.

--- src/Funcoes.java
+++ src/Funcoes.java
@@ -1,18 +1,10 @@
 public class Funcoes {
-    //public static void main (String [] args){
-        //String nomeOriginal = "Janier Silva";
-        //saudacao(nomeOriginal);
 
     public static void main(String [] args){
-        int resultado = soma(2, 3);
+        int resultado = soma(20, 200);
         System.out.println(resultado);
     }
-    //public static void saudacao(String nomeParametro){
-
-        //System.out.println("Hello, " + nomeParametro);
-
     public static int soma(int a, int b){
         return a + b;
+        }
     }
-    }
-
